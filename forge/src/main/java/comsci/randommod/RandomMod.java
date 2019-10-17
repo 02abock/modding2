82b8f873ea3d834569e401a40c31499d4e1b3c43
A.bock <A.bock@RHS193-2002266.rhs193.org>
2019-10-17 14:47:15 -0400

    first item in proggress

--- forge/src/main/java/comsci/randommod/RandomMod.java
+++ forge/src/main/java/comsci/randommod/RandomMod.java
@@ -3,7 +3,13 @@ package comsci.randommod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import comsci.randommod.list.ItemList;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -34,6 +40,22 @@ public class RandomMod
 	private void clientRegistries(final FMLClientSetupEvent event) 
 	{
 		logger.info("clientRegisties method registered");
+	}
+	
+	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents
+	{
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event)
+		{
+			ItemList.tutorial_item=new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("tutorial_item"));
+			
+			logger.info("Items registered.");
+		}
+		
+		private static ResourceLocation location(String name) {
+			return new ResourceLocation(modid,name);
+		}
 	}
 	
 }
---
+++ forge/src/main/java/comsci/randommod/list/ItemList.java
@@ -0,0 +1,9 @@
+package comsci.randommod.list;
+
+import net.minecraft.item.Item;
+
+public class ItemList 
+{
+	public static Item tutorial_item;
+
+}
